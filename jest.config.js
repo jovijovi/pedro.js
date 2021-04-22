d331708d8b4623a603307d42ee4b3b6c0aef5a5a
jovijovi <mageyul@hotmail.com>
2021-04-22 16:26:28 +0800

    chore: use relative path

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,8 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  moduleNameMapper: {
-    '@common/(.*)': '<rootDir>/common/$1',
-    '@network/(.*)': '<rootDir>/network/$1'
-  }
+  // moduleNameMapper: {
+  //   '@common/(.*)': '<rootDir>/common/$1',
+  //   '@network/(.*)': '<rootDir>/network/$1'
+  // }
 };
