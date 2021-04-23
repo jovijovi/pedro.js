420497bcda96966b62fb751820b1aa684b1686c5
jovijovi <mageyul@hotmail.com>
2021-04-23 20:05:40 +0800

    feat: add 'sys' package to handle system signals

---
+++ lib/common/sys/sys.ts
@@ -0,0 +1,31 @@
+import * as log from '../log';
+import * as time from '../util/time';
+
+export const SIGINT = 'SIGINT';
+export const SIGTERM = 'SIGTERM';
+export const SIGHUP = 'SIGHUP';
+
+const Signals = [
+	SIGINT,
+	SIGTERM,
+	SIGHUP,
+];
+
+function shutdown() {
+	log.Close(() => {
+			console.log("[%s] ### Logger close done.", time.GetUTCTimeStamp(), time.GetLocalTimeStamp());
+			process.abort();
+		}
+	);
+}
+
+function handle(signal) {
+	log.RequestId().info("PID=%d, Signal=%s, system exit.", process.pid, signal);
+	shutdown();
+}
+
+export function HandleSignals() {
+	Signals.forEach(sig => {
+		process.on(sig, handle);
+	})
+}
