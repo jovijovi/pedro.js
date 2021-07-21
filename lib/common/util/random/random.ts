309eec6a9b7fa086b2bf88dd8a66cd1f9a6e309f
jovijovi <mageyul@hotmail.com>
2021-07-21 17:55:33 +0800

    feat: RandBoolean, RandCase, RandUppercase, RandLowercase

--- lib/common/util/random/random.ts
+++ lib/common/util/random/random.ts
@@ -4,3 +4,53 @@ import * as crypto from 'crypto';
 export function RandIntBetween(min, max: number): number {
 	return crypto.randomInt(min, max);
 }
+
+// RandBoolean random boolean, range: [true, false]
+export function RandBoolean(): boolean {
+	return RandIntBetween(0, 2) > 0;
+}
+
+// RandCase random letter case of string
+export function RandCase(input: string): string {
+	let output = '';
+
+	for (let i = 0; i < input.length; i++) {
+		if (RandBoolean()) {
+			output += input[i].toUpperCase();
+		} else {
+			output += input[i].toLowerCase();
+		}
+	}
+
+	return output;
+}
+
+// RandUppercase random letter uppercase of string
+export function RandUppercase(input: string): string {
+	let output = '';
+
+	for (let i = 0; i < input.length; i++) {
+		if (RandBoolean()) {
+			output += input[i].toUpperCase();
+		} else {
+			output += input[i];
+		}
+	}
+
+	return output;
+}
+
+// RandLowercase random letter lowercase of string
+export function RandLowercase(input: string): string {
+	let output = '';
+
+	for (let i = 0; i < input.length; i++) {
+		if (RandBoolean()) {
+			output += input[i].toLowerCase();
+		} else {
+			output += input[i];
+		}
+	}
+
+	return output;
+}
