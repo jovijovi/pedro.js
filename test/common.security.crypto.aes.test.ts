c5b6398afb0ef9a8c1a7a6dd400a619d76a8358a
jovijovi <mageyul@hotmail.com>
2021-07-21 15:13:35 +0800

    test: add aes test code

---
+++ test/common.security.crypto.aes.test.ts
@@ -0,0 +1,15 @@
+import * as assert from 'assert';
+import * as aes from "../lib/common/security/crypto/aes";
+
+test('common.security.crypto.aes', () => {
+	const mockUserKey = "this is a user key 1234567890";
+	const mockPlainText = "Hello, world! 1234567890 ~!@#$%^&*()_+";
+
+	const cipherText = aes.Encrypt(mockUserKey, mockPlainText);
+	console.log("cipherText=", cipherText);
+
+	const plainText = aes.Decrypt(mockUserKey, cipherText);
+	console.log("plainText=", plainText);
+
+	assert.strictEqual(plainText, mockPlainText);
+})
