0fb4d3d49218b59efd4ea8d74aedb22e828e8aea
jovijovi <mageyul@hotmail.com>
2021-09-29 14:36:36 +0800

    feat: (common/version) func 'GetVersionInfo' returns version info

--- lib/common/version/version.ts
+++ lib/common/version/version.ts
@@ -1,1 +1,21 @@
-// TODO: Application Binary Version Info
+import * as fs from 'fs'
+
+export namespace Version {
+	const versionInfoFilename = 'git.json';
+
+	let versionInfo: any;
+
+	// GetVersionInfo returns application version info
+	export function GetVersionInfo(): any {
+		try {
+			if (!versionInfo) {
+				versionInfo = JSON.parse(fs.readFileSync(versionInfoFilename, 'utf8'));
+				return versionInfo;
+			}
+
+			return versionInfo;
+		} catch (e) {
+			return {};
+		}
+	}
+}
