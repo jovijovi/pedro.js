98076bdb1f08e7bda1e56033b60d0a868e493521
jovijovi <mageyul@hotmail.com>
2021-09-29 14:38:04 +0800

    feat: (taskhandler/healthcheck) return version info

--- lib/taskhandler/healthcheck/health.ts
+++ lib/taskhandler/healthcheck/health.ts
@@ -1,6 +1,9 @@
 import * as log from '../../common/log';
+import * as response from '../../network/http/response';
+import {Version} from '../../common/version';
 
 export function Health(req, res) {
-	res.send('OK');
-	log.RequestId().info("Health");
+	res.send(response.BuildResponse("200", "SuccessfulOperation", Version.GetVersionInfo()));
+
+	log.RequestId().trace("Receive HealthCheck. URL.Path=", req.request.url);
 }
