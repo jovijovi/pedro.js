b0372a0794c57485b2e8014114b85be816376a3b
jovijovi <mageyul@hotmail.com>
2021-09-30 16:25:11 +0800

    feat: returns metrics info

--- lib/taskhandler/metricscheck/metrics.ts
+++ lib/taskhandler/metricscheck/metrics.ts
@@ -1,6 +1,8 @@
 import * as log from '../../common/log';
+import {Metrics as mi} from "../../common/metrics";
+import * as response from "../../network/http/response";
 
 export function Metrics(req, res) {
-	res.send('Metrics');
-	log.RequestId().info("Metrics");
+	res.send(response.BuildResponse("200", "SuccessfulOperation", JSON.parse(mi.GetMetricsInfo().toJSON())));
+	log.RequestId().info("Receive MetricsCheck. URL.Path=", req.url);
 }
