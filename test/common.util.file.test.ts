a10c9992f6f2832b0b4dfdf25896ddc5436fc1ae
jovijovi <mageyul@hotmail.com>
2021-10-11 15:57:41 +0800

    feat: (common/util/file) test code

---
+++ test/common.util.file.test.ts
@@ -0,0 +1,6 @@
+import * as file from '../lib/common/util/file';
+
+test('ReadDedupeLineToMap', async () => {
+	let map = await file.ReadDedupeLineToMap('./test/mock/dedupe_line_file.txt');
+	console.log("Map=", map);
+})
