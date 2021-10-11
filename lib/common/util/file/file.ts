be9d2355f46e72a5a9f7292f505f7a05c7327489
jovijovi <mageyul@hotmail.com>
2021-10-11 15:57:12 +0800

    feat: (common/util/file) read dedupe line to map

---
+++ lib/common/util/file/file.ts
@@ -0,0 +1,29 @@
+import readline from 'readline';
+import fs from 'fs';
+import * as digest from '../../security/crypto/digest';
+
+// ReadDedupeLineToMap returns dedupe line from the file
+export async function ReadDedupeLineToMap(filename: string): Promise<Map<any, any>> {
+	if (!filename) {
+		return null;
+	}
+
+	const rl = readline.createInterface({
+		input: fs.createReadStream(filename, 'utf8'),
+		crlfDelay: Infinity,
+	});
+
+	if (!rl) {
+		return null;
+	}
+
+	let lineMap = new Map();
+
+	for await (const line of rl) {
+		lineMap.set(digest.GetString(line, 'sha256'), line);
+	}
+
+	rl.close();
+
+	return lineMap;
+}
