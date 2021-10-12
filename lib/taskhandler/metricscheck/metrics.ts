9fedce2592313283474a2c877cd3e478fda63034
jovijovi <mageyul@hotmail.com>
2021-10-12 14:01:45 +0800

    chore: reformat code

--- lib/taskhandler/metricscheck/metrics.ts
+++ lib/taskhandler/metricscheck/metrics.ts
@@ -1,6 +1,6 @@
 import * as log from '../../common/log';
-import {Metrics as mi} from "../../common/metrics";
-import * as response from "../../network/http/response";
+import {Metrics as mi} from '../../common/metrics';
+import * as response from '../../network/http/response';
 
 export function Metrics(req, res) {
 	res.send(response.BuildResponse("200", "SuccessfulOperation", JSON.parse(mi.GetMetricsInfo().toJSON())));
