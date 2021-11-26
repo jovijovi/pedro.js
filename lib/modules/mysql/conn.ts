ea7d472e25176a3169a5c50af57757fc0131b01c
jovijovi <mageyul@hotmail.com>
2021-11-26 18:13:24 +0800

    fix: (modules/mysql) type boolean trivially inferred from a boolean literal, remove type annotation

--- lib/modules/mysql/conn.ts
+++ lib/modules/mysql/conn.ts
@@ -12,7 +12,7 @@ export namespace Mysql {
 	}
 
 	// Connect mysql database
-	export function Connect(cfg: Config, logging: boolean = false): Sequelize {
+	export function Connect(cfg: Config, logging = false): Sequelize {
 		if (cfg.uri) {
 			return new Sequelize(cfg.uri, {
 				logging: logging,
