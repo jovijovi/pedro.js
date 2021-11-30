871c81741c9cefe9c958cf795739b33935c4d727
jovijovi <mageyul@hotmail.com>
2021-11-30 14:32:31 +0800

    feat: (network/http/response) add more response builders

--- lib/network/http/response/response.ts
+++ lib/network/http/response/response.ts
@@ -1,5 +1,7 @@
 // Response Builder
 
+import * as log from '../../../common/log';
+
 // BuildResponse returns response by params
 export function BuildResponse(code: string, msg: string, body: any): any {
 	return {
@@ -7,4 +9,32 @@ export function BuildResponse(code: string, msg: string, body: any): any {
 		msg: msg,
 		body: body
 	};
+}
+
+export function BadRequest(res): any {
+	return res.status(400).send({
+		error: 'Bad request',
+	});
+}
+
+export function NotFound(res): any {
+	return res.status(404).send({
+		error: 'Not found',
+	});
+}
+
+export function Error(res, e): any {
+	if (e && e.code && e.message) {
+		log.RequestId().error("code=%s, message=%s", e.code, e.message);
+	}
+
+	let httpStatusCode = 500;
+	if (e.message.toString().includes('Not found')) {
+		httpStatusCode = 404;
+	}
+
+	return res.status(httpStatusCode).send({
+		code: e.code,
+		error: e.message,
+	});
 }
