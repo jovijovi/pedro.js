52a3fac63bac8b66786aa0075ba24d72c1bb6bf0
jovijovi <mageyul@hotmail.com>
2022-01-26 15:29:43 +0800

    test(common/util/octopus): create octopus/cube/water

--- test/common/common.util.octopus.test.ts
+++ test/common/common.util.octopus.test.ts
@@ -41,9 +41,94 @@ test('Iterator', () => {
 	o.Push("5");
 
 	let count = 0;
-	for (const n of o) {
+	for (const n of o.Entries()) {
 		console.log("n=", n.payload.value);
 		count++;
 	}
 	assert.strictEqual(count, 5);
+})
+
+/*
+       x.3
+       /
+     x.2
+     /
+   x.1
+   /
+  1 --- 2 -- y.1 -- y.2
+  |     |
+  |     |
+  4 --- 3 -- z.1 -- z.2 -- z.3 -- z.4
+   \
+   alpha.1
+     \
+     alpha.2 -- beta.1
+*/
+test('Create a big octopus', () => {
+	const octopus = new Octopus("1");
+	octopus.Push("2").Append("y.1", "y").Append("y.2");
+	const cell3 = octopus.Push("3");
+	const cell4 = octopus.Push("4");
+
+	octopus.Head().Append("x.1", "x").Append("x.2").Append("x.3");
+	cell3.Append("z.1", "z").Append("z.2").Append("z.3").Append("z.4");
+
+	const alpha2 = cell4.Append("alpha.1", "alpha").Append("alpha.2");
+	alpha2.Append("beta.1", "beta");
+
+	console.log("BigOctopus=%o", octopus);
+	for (const cell of octopus.Entries()) {
+		console.log("Cell=%o", cell);
+	}
+
+	for (const n of alpha2.Entries()) {
+		console.log("n=%o", n);
+	}
+
+	for (const n of alpha2.Entries("beta")) {
+		console.log("n=%o", n);
+	}
+})
+
+/*
+     e--------f
+    /|       /|
+   / |      / |
+  a--------b  |
+  |  |     |  |
+  |  h-----|--g
+  | /      | /
+  |/       |/
+  d--------c
+*/
+test('Create a cube', () => {
+	const cube = new Octopus('a');
+	const e = cube.Head().Append('e', 'ae');
+	const f = cube.Push('b').Append('f', 'bf');
+	const g = cube.Push('c').Append('g', 'cg');
+	const h = cube.Push('d').Append('h', 'dh');
+
+	e.Link(f, 'ef');
+	f.Link(g, 'fg');
+	g.Link(h, 'gh');
+	h.Link(e, 'he');
+
+	console.log("Cube=%o", cube);
+})
+
+/*
+  H   H
+   \ /
+    O
+*/
+test('Create a water molecule (H2O)', () => {
+	const water = new Octopus('O');
+	water.Head().Append('H', 'left H bond');
+	water.Head().Append('H', 'right H bond');
+
+	console.log("Water=%o", water);
+
+	for (let atom of water.Head().Links()) {
+		console.log("Atom=%o", atom);
+	}
 })
