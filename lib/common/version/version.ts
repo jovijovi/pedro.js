32ebca6f2d3ddda242104b97ea077a2ae49201b6
jovijovi <mageyul@hotmail.com>
2022-03-03 14:23:08 +0800

    refactor(common/version): code improved

--- lib/common/version/version.ts
+++ lib/common/version/version.ts
@@ -2,20 +2,28 @@ import * as fs from 'fs'
 
 export namespace Version {
 	const versionInfoFilename = 'git.json';
+	const unknown = 'unknown';
 
 	let versionInfo: any;
 
-	// GetVersionInfo returns application version info
+	// GetVersionInfo returns app version info from git.json
+	// git.json example:
+	// {
+	// 	"version": "dev.17585bd",
+	// 	"gitCommit": "17585bd",
+	// 	"buildTime": "20220301",
+	// 	"tsVersion": "Version 4.6.2"
+	// }
 	export function GetVersionInfo(): any {
 		try {
-			if (!versionInfo) {
-				versionInfo = JSON.parse(fs.readFileSync(versionInfoFilename, 'utf8'));
-				return versionInfo;
-			}
-
-			return versionInfo;
+			return versionInfo ? versionInfo : JSON.parse(fs.readFileSync(versionInfoFilename, 'utf8'));
 		} catch (e) {
-			return {};
+			return {
+				version: unknown,
+				gitCommit: unknown,
+				buildTime: unknown,
+				tsVersion: unknown
+			};
 		}
 	}
 }
