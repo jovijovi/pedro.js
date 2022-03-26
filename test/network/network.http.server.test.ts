07bbb5af24f2d59cc9fb56bbce1efc56c24cea40
jovijovi <mageyul@hotmail.com>
2022-03-26 16:55:45 +0800

    test(network/http): remove useless test code

--- test/network/network.http.server.test.ts
+++ test/network/network.http.server.test.ts
@@ -1,7 +1,32 @@
-import {config} from '@jovijovi/pedrojs-common';
-import {http} from '../../lib/network';
+import {config, log} from '@jovijovi/pedrojs-common';
+import * as core from 'express-serve-static-core';
+import {http} from '../../lib/network'
+import {ITaskHandler} from '../../lib/network/http';
 
-test('HttpServer', () => {
+function biz1(req, res) {
+	log.RequestId().info("Req=", req);
+	res.send('biz1');
+}
+
+async function biz2(req, res) {
+	log.RequestId().info("Req=", req);
+	res.send('biz2');
+}
+
+function registerBizAPI(router: core.Express) {
+	router.get('/api/v1/biz1', biz1);
+	router.get('/api/v1/biz2', biz2);
+}
+
+class privateImplHandlers implements ITaskHandler {
+	RegisterHandlers(router: core.Express) {
+		registerBizAPI(router);
+	}
+}
+
+const AppHandlers = new privateImplHandlers();
+
+test('TaskHandler', () => {
 	config.LoadConfig('./conf/app.config.yaml');
-	http.server.Run();
+	http.server.Run(AppHandlers);
 })
