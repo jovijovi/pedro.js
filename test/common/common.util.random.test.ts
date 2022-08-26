71531a210adf0cda0693617e4b820c1fb8e26a9a
jovijovi <mageyul@hotmail.com>
2022-08-26 15:40:49 +0800

    test(common/util/random): more targeted UT code

--- test/common/common.util.random.test.ts
+++ test/common/common.util.random.test.ts
@@ -3,9 +3,9 @@ import * as assert from 'assert';
 
 test('RandIntBetween', () => {
 	for (let i = 0; i < 10; i++) {
-		const result = util.random.RandIntBetween(0, 3);
+		const result = util.random.RandIntBetween(-3, 3);
 		assert.notStrictEqual(result, 3);
-		log.RequestId().info(util.random.RandIntBetween(0, 3));
+		log.RequestId().info(util.random.RandIntBetween(-3, 3));
 	}
 })
 
@@ -32,21 +32,21 @@ test('RandBoolean', () => {
 
 test('RandCase', () => {
 	for (let i = 0; i < 10; i++) {
-		const result = util.random.RandCase('a0b1c2d3e4f5. "测试');
+		const result = util.random.RandCase('a0b1c2d3e4f5. TEST"测试');
 		log.RequestId().info(result);
 	}
 })
 
 test('RandUppercase', () => {
 	for (let i = 0; i < 10; i++) {
-		const result = util.random.RandUppercase('a0b1c2d3e4f5. "测试');
+		const result = util.random.RandUppercase('a0b1c2d3e4f5. тест"テスト');
 		log.RequestId().info(result);
 	}
 })
 
 test('RandLowercase', () => {
 	for (let i = 0; i < 10; i++) {
-		const result = util.random.RandLowercase('A0B1C2D3E4F5. "测试');
+		const result = util.random.RandLowercase('A0B1C2D3E4F5. Ensayo"테스트');
 		log.RequestId().info(result);
 	}
 })
